4dbb9177a44d00f8f661fea89ae0b1f231ca63e1
SoyalAlikhan <soyalkhan53133@gmail.com>
2025-01-15 08:54:40 +0000

    variables of js

---
+++ javaScript_Bacic/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 123456;
+let accountName = "soyal khan"
+var accountPassword = 987654322
+accountEmail = "soyalkhan@gmail.com"
+
+console.table([accountId,accountName,accountPassword,accountEmail])
