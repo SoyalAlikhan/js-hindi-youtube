ff056788ae87d90ea84ed9b40f28fbc4fbb3b323
SoyalAlikhan <soyalkhan53133@gmail.com>
2025-01-15 08:55:43 +0000

    datatype conversion in js

---
+++ javaScript_Bacic/03_datatype_conversion.js
@@ -0,0 +1,23 @@
+let score = "34nkbk"
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+// "33" => 33
+// "333jjj" => NaN
+// true => 1; false => 0
+
+let isLoggedIn ="soyal"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn)
+
+// "" => false
+// "soyal" => true
+// 1 => true; 0 => false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
