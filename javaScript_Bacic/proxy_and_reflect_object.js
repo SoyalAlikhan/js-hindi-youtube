a79f36b718901c2d00df9e5e91533162c6843fa5
SoyalAlikhan <soyalkhan53133@gmail.com>
2025-01-18 20:05:50 +0000

    proxy and reflect

--- javaScript_Bacic/proxy_and_reflect_object.js
+++ javaScript_Bacic/proxy_and_reflect_object.js
@@ -8,7 +8,8 @@ const p1 = {
 // console.log(p1.age)
 const p1Proxy = new Proxy(p1 , {
     get(target , prop){
-        if(prop in target) return target[prop];
+        //if(prop in target) return target[prop];
+        if(prop in target) return Reflect.get(target,prop);
         return false;
     },
     set(target, prop, value){
@@ -25,11 +26,37 @@ const p1Proxy = new Proxy(p1 , {
                 if(value <= 0) throw new  Error(`${prop} value must be > 0`)
 
         }
-        target[prop] = value;
+        //target[prop] = value;
+        Reflect.set(target,prop,value)
     },  
 });
 
 p1Proxy.age =25
 p1Proxy.fname = 'Soyal'
 
-console.log(p1Proxy)
+//console.log(p1.age)
+
+const student = {
+    name : 'Anjum Fatima',
+    age : 21
+}
+
+const proxyStudent = new Proxy(student,{
+    get(target,prop){
+        // console.log(`${prop} proxy is executed`);
+        // return Reflect.get(target,prop);
+        if(prop === "age"){
+            return `${target[prop]} saal ka h`
+        }else if(prop === "name"){
+            return `${target[prop]} ye mera naam h`
+        }else{
+            return target[prop];
+        }
+    }
+});
+
+console.log(proxyStudent.age)
+console.log(proxyStudent.name)
+console.log(Reflect.get(student,'name')) // Property ko read karna using Reflect
+Reflect.set(student,"age",22) // Property ko update karna using Reflect
+console.log(student.age)
