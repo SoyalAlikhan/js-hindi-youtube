4a2a0e8003783296630f1e435ead5d4d4d5ec997
SoyalAlikhan <soyalkhan53133@gmail.com>
2025-04-14 10:25:31 +0000

    change

---
+++ javaScript_Basic/05_iterations/eight.js
@@ -0,0 +1,29 @@
+const myNums = [1,2,3,4]
+
+const myTotal = myNums.reduce(function(acc,curr){
+    console.log(`accumalator: ${acc} and currentvalue: ${curr}`);
+    return acc + curr;
+},0)
+console.log(myTotal);
+
+const myTotal1 = myNums.reduce((acc,curr)=> acc+curr,0)
+console.log(myTotal1);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2000
+    },
+    {
+        itemName: "mobile course",
+        price: 3000
+    },
+    {
+        itemName: "py course",
+        price: 4000
+    }
+]
+
+ const totalPriceOfAllCourse = shoppingCart.reduce( (acc,item) => acc + item.price ,0 );
+ console.log(`Total Price Of All Course Is: ${totalPriceOfAllCourse}`);
+ 
