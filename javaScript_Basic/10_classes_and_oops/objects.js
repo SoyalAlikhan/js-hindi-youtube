d47102911d4d74dc4937b9133ebbade00d6f185b
SoyalAlikhan <soyalkhan53133@gmail.com>
2025-04-26 09:27:35 +0000

    object changes

--- javaScript_Basic/10_classes_and_oops/objects.js
+++ javaScript_Basic/10_classes_and_oops/objects.js
@@ -23,8 +23,72 @@ createUser.prototype.printMe = function () {
 const user1 = new createUser('soyal',10)
 const user2 = new createUser('khan',20)
 
-user1.increment();
-user1.printMe()
+// user1.increment();
+// user1.printMe()
+
+const heros = ["thor","spiderman"]
+
+const herosPower = {
+    thor:"hammer",
+    spiderman:"spidy",
+    spidermanPower : function () {
+        console.log(`spiderman is  power ${this.spiderman}`);
+
+    }
+}
+
+Object.prototype.soyal = function () {
+    console.log('i am available for all objects');
+    
+}
+
+Array.prototype.khan = function() {
+    console.log(`i am available for all array`);
+    
+}
+// herosPower.soyal()
+// heros.soyal()
+//heros.khan()
+//herosPower.khan()   //error
 
 
+//old inheritance technic
 
+const User = {
+    username:'soyal'
+}
+const Teacher = {
+    makeVideo: true
+}
+const TeacherSuport = {
+    isAvailable: true
+}
+const TASupport = {
+    makeAssignment: 'Js assignment',
+    fullTime:true,
+    __proto__:TeacherSuport   //inherite TeacherSuport
+}
+// console.log(TASupport.isAvailable);
+// console.log(TASupport.makeAssignment);
+
+Teacher.__proto__ = User  // inherite User
+// console.log(Teacher.username);
+// console.log(Teacher.makeVideo);
+
+//new or modern Syntax of inheritance
+
+Object.setPrototypeOf(TeacherSuport,Teacher) //inherit Teacher object in TeacherSupport
+// console.log(TeacherSuport.makeVideo);
+// console.log(TeacherSuport.isAvailable);
+
+const userName = "soyalkhan    "
+console.log(userName.length);
+
+String.prototype.tureLength = function(){
+    console.log(`${this}`);
+    console.log(`${this.trim().length}`);
+}
+userName.tureLength()
+
+"khan".tureLength()
+"ALi".tureLength()
